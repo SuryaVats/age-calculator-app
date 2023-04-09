282df8e56c2f575a68032a907a308cd6cb2a5eba
Sagar Jaswal <sagarjaswal9102001@gmail.com>
2023-04-09 16:28:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello</div>
+    <div className="container">
+      {/* Input */}
+      <div className='inputs'>
+        <div className='input_container'>
+          <label htmlFor="">DAY</label>
+          <input className='inp' type="number" id='day' />
+        </div>
+
+        <div className='input_container'>
+          <label htmlFor="">MONTH</label>
+          <input className='inp' type="number" id='month' />
+        </div>
+
+        <div className='input_container'>
+          <label htmlFor="">YEAR</label>
+          <input className='inp' type="number" id='year' />
+        </div>
+      </div>
+
+      {/* Button */}
+      <div className='submitButton'></div>
+
+      {/* Outputs */}
+      <div className="outputs">
+        <h2><span>{32}</span> years</h2>
+        <h2><span>{12}</span> months</h2>
+        <h2><span>{24}</span> days</h2>
+      </div>
+    </div>
+
   );
 }
 
