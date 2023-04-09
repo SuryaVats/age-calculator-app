10b383123b6186d74b98f22a0e0d85332a9d4edf
Sagar Jaswal <sagarjaswal9102001@gmail.com>
2023-04-09 18:20:12 +0530

    layout and designing, responsivity yet to be done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import './App.css';
+import arrowIcon from './assets/images/icon-arrow.svg'
+
 
 function App() {
+
   return (
     <div className="container">
       {/* Input */}
-      <div className='inputs'>
-        <div className='input_container'>
-          <label htmlFor="">DAY</label>
-          <input className='inp' type="number" id='day' />
-        </div>
+      <div className="wrapper">
+        <form className='form'>
+          <div className="inputs">
+            <div className='input_container'>
+              <label htmlFor="day">DAY</label>
+              <input className='inp' type="number" id='day' placeholder='DD' />
+            </div>
 
-        <div className='input_container'>
-          <label htmlFor="">MONTH</label>
-          <input className='inp' type="number" id='month' />
-        </div>
+            <div className='input_container'>
+              <label htmlFor="month">MONTH</label>
+              <input className='inp' type="number" id='month' placeholder='MM' />
+            </div>
 
-        <div className='input_container'>
-          <label htmlFor="">YEAR</label>
-          <input className='inp' type="number" id='year' />
-        </div>
+            <div className='input_container'>
+              <label htmlFor="year">YEAR</label>
+              <input className='inp' type="number" id='year' placeholder='YYYY' />
+            </div>
+          </div>
+          {/* Button */}
+          <div className='submitButton'>
+            <div className="line"></div>
+            <button> <img src={arrowIcon} alt='Submit' /></button>
+          </div>
+        </form >
+
+
+        {/* Outputs */}
+        < div className="outputs" >
+          <h2><span>{32}</span> years</h2>
+          <h2><span>{12}</span> months</h2>
+          <h2><span>{24}</span> days</h2>
+        </div >
       </div>
 
-      {/* Button */}
-      <div className='submitButton'></div>
-
-      {/* Outputs */}
-      <div className="outputs">
-        <h2><span>{32}</span> years</h2>
-        <h2><span>{12}</span> months</h2>
-        <h2><span>{24}</span> days</h2>
-      </div>
-    </div>
+    </div >
 
   );
 }
